68a729a5a36f9bb7a90b7b673fcbfdb67778ab12
Blubas35 <dtevelis@gmail.com>
2023-03-27 15:32:22 +0300

    porftolio website created

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 
 const About = () => {
-  return (
-    <div name='about'>
-        
-    </div>
-  )
+    return (
+        <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+            <div className='flex flex-col justify-center items-center w-full h-full'>
+                <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+                    <div className='sm:text-right pb-8 pl-4'>
+                        <p className='text-4xl font-bold inline border-b-4 border-pink-600'>About</p>
+                    </div>
+                    <div></div>
+                </div>
+                <div className='max-w-[1000px] w-full sm:grid grid-cols-2 gap-8 px-4'>
+                    <div className='sm:text-right text-4-xl font-bold'>
+                        <p>Hi. I am Dom, nice to meet you. Please take a look around</p>
+                    </div>
+                    <div>
+                        <p>I am passionate about building....</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default About
