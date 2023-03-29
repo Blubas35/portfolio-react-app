fc57c94383765b8ef43035f63d817436abdda6ca
Blubas35 <dtevelis@gmail.com>
2023-03-29 11:30:39 +0300

    logo added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook } from 'react-icons/fa'
 import { HiOutlineMail } from 'react-icons/hi'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
-import Logo from '../assets/logo.png'
+import Logo from '../assets/dt-logo.png'
 import { Link } from 'react-scroll'
 
 
